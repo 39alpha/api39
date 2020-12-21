f5f668b5a46974bbb1ffa4998c038950f600f998
Douglas G. Moore <doug@39alpharesearch.org>
2020-12-21 15:55:13 -0700

    Implement basic git repo management

--- main.go
+++ main.go
@@ -17,12 +17,14 @@ var (
 	port       = 3964
 	genconf    = false
 	configpath = ""
+    gitbox = false
 )
 
 func init() {
 	flag.IntVar(&port, "port", port, "port on which the server will listen")
 	flag.BoolVar(&genconf, "genconf", genconf, "generate and print a configuration file to STDOUT and exit")
 	flag.StringVar(&configpath, "config", configpath, "path to configuration file (required)")
+    flag.BoolVar(&gitbox, "gitbox", gitbox, "fiddling with git2go")
 }
 
 func main() {
@@ -34,6 +36,12 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+    } else if gitbox {
+        remote := "https://github.com/39alpha/39alpharesearch.org"
+        path := "/home/dgm/website"
+        if err := api39.UpdateGitRepo(remote, path); err != nil {
+            log.Fatalf("Failed to Update Repo: %v\n", err)
+        }
 	} else {
 		if configpath == "" {
 			fmt.Fprintf(os.Stderr, "Error: -config flag is required\n\n")

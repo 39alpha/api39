0e312903c3fc86626e0069e790c5ef39f57274ac
Douglas G. Moore <doug@dglmoore.com>
2022-07-21 01:40:40 -0700

    Run build in a go routine

--- api39/site/update.go
+++ api39/site/update.go
@@ -43,7 +43,7 @@ func UpdateDNSLink(cfg *api39.Config, ipfshash string) error {
 	if err != nil {
 		return err
 	} else if res.StatusCode != 200 {
-		return fmt.Errorf("failed to set _dnslink (%s)", res.Status)
+		return fmt.Errorf("%s", res.Status)
 	}
 
 	return nil;
@@ -62,57 +62,43 @@ func Update(ctx iris.Context) {
 		log.Printf("Request Recieved: branch is not main (%v)\n", ref.(string))
 		_, _ = ctx.JSON(iris.Map{"message": "successful request"})
 	} else {
-		cfg, ok := ctx.Values().Get("config").(*api39.Config)
-		if !ok {
-			log.Println("failed to retrieve configuration from context")
-			ctx.StopWithJSON(iris.StatusInternalServerError, iris.Map{
-				"message": "failed to retrieve configuration from context",
-			})
-			return
-		}
+		go func() {
+			cfg, ok := ctx.Values().Get("config").(*api39.Config)
+			if !ok {
+				log.Println("failed to retrieve configuration from context")
+				return
+			}
 
-		if err := api39.UpdateGitRepo(cfg.Site.Repo, cfg.Site.Path); err != nil {
-			message := "failed to update repository"
-			log.Printf("%s: %v\n", message, err)
-			ctx.StopWithJSON(iris.StatusInternalServerError, iris.Map{
-				"message": message,
-				"error":   err,
-			})
-			return
-		}
+			if err := api39.UpdateGitRepo(cfg.Site.Repo, cfg.Site.Path); err != nil {
+				message := "failed to update repository"
+				log.Printf("%s: %v\n", message, err)
+				return
+			}
 
-		if err := api39.RebuildWithMake(cfg.Site.Path); err != nil {
-			message := "failed to rebuild site"
-			log.Printf("%s: %v\n", message, err)
-			ctx.StopWithJSON(iris.StatusInternalServerError, iris.Map{
-				"message": message,
-				"error":   err,
-			})
-			return
-		}
+			if err := api39.RebuildWithMake(cfg.Site.Path); err != nil {
+				message := "failed to rebuild site"
+				log.Printf("%s: %v\n", message, err)
+				return
+			}
 
-		path := filepath.Join(cfg.Site.Path, "public")
-		hash, err := api39.IpfsAddDir(cfg.Ipfs.Url, path)
-		if err != nil {
-			message := "failed to add site to IPFS"
-			log.Printf("%s: %v\n", message, err)
-			ctx.StopWithJSON(iris.StatusInternalServerError, iris.Map{
-				"message": message,
-				"error":   err,
-			})
-			return
-		}
+			path := filepath.Join(cfg.Site.Path, "public")
+			hash, err := api39.IpfsAddDir(cfg.Ipfs.Url, path)
+			if err != nil {
+				message := "failed to add site to IPFS"
+				log.Printf("%s: %v\n", message, err)
+				return
+			}
 
-		log.Printf("New IPFS Hash: %s\n", hash)
+			log.Printf("New IPFS Hash: %s\n", hash)
 
-		err = UpdateDNSLink(cfg, hash)
-		if err != nil {
-			ctx.StopWithJSON(iris.StatusInternalServerError, iris.Map{
-				"message": "failed to update DNS settings",
-				"error": err,
-			})
-		}
+			err = UpdateDNSLink(cfg, hash)
+			if err != nil {
+				message := "failed to update DNS records"
+				log.Printf("%s: %v\n", message, err)
+				return
+			}
+		}()
 
-		_, _ = ctx.JSON(iris.Map{"message": "successful", "hash": hash})
+		_, _ = ctx.JSON(iris.Map{"message": "successful"})
 	}
 }
